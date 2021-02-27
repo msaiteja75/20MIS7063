fda32a8c1accfb106de26e7647b41e64536bc062
msaiteja75 <79200279+msaiteja75@users.noreply.github.com>
2021-02-27 20:45:38 +0530

    Create Simple.java

---
+++ Simple.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Simple{
+public static void main(String args[]){
+System.out.println("Enter any three values ");
+Scanner s = new Scanner(System.in);
+int a = s.nextInt();
+int b = s.nextInt();
+int c = s.nextInt();
+int sum = a+b+c;
+System.out.println(+sum);
+}
+}
